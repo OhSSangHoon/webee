4b81caeaaf09db90da1e60885e0d487c684657a5
yun yeo joung <da.young.yun13@gmail.com>
2025-06-25 00:27:19 +0900

    feat:날씨 위젯 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
-import {
-  DiagnosisBanner,
-  QuickProfile,
-  RecommendBee,
-  Weather,
-} from "@/widgets";
+import { Weather2 } from "@/features";
+import { DiagnosisBanner, QuickProfile } from "@/widgets";
 import QuickMenu from "@/widgets/home/quickMenu/ui/quickMenu";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 max-w-screen-xl mx-auto px-4">
       <div className="flex flex-row gap-4">
-        <RecommendBee /> <QuickProfile />
+        <Weather2 /> <QuickProfile />
       </div>
-      <Weather />
+
       <div className="flex flex-row gap-4">
         <QuickMenu />
         <DiagnosisBanner />
