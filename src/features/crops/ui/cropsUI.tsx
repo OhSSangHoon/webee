28b18e3093c29347d1655abb7fa0ac3230836304
yun yeo joung <da.young.yun13@gmail.com>
2025-06-27 00:53:15 +0900

    Merge branch 'feature/recommendBee' of https://github.com/fieldnight/weBeeWeb into feature/recommendBee

--- src/features/crops/ui/cropsUI.tsx
+++ src/features/crops/ui/cropsUI.tsx
@@ -23,9 +23,21 @@ export default function Crops({ onSelect }: Props) {
     updateCrop,
     deleteCrop,
     setError,
-  } = useCrops();
+    fetchCrops,
+  } = useCrops(true); //lazy
 
   const [isOpen, setIsOpen] = useState(false);
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const hasAccessToken = !!localStorage.getItem("accessToken");
+
+  const openModal = () => {
+    setIsOpen(true);
+    if (!hasAccessToken) return; // at 없으면 막기
+    if (!shouldLoad) {
+      fetchCrops(); // 모달 최초 열릴 때만 API 호출
+      setShouldLoad(true);
+    }
+  };
 
   const closeModal = () => {
     setIsOpen(false);
@@ -44,10 +56,20 @@ export default function Crops({ onSelect }: Props) {
   return (
     <div className="w-full">
       <button
-        onClick={() => setIsOpen(true)}
-        className="border border-blue-500 hover:bg-blue-500 hover:text-white text-blue-500 px-4 py-2 rounded-md w-full"
+        onClick={openModal}
+        disabled={!hasAccessToken} // 중복 막기
+        className={`relative w-full px-4 py-2 rounded-md border text-blue-500 ${
+          hasAccessToken
+            ? "border-blue-500 hover:bg-blue-500 hover:text-white cursor-pointer"
+            : "border-gray-300 bg-gray-100 text-gray-400 cursor-not-allowed"
+        }`}
       >
         기존 작물 정보 가져오기
+        {!hasAccessToken && (
+          <p className=" absolute mt-2 text-sm text-red-500">
+            로그인 시 사용 가능합니다.
+          </p>
+        )}
       </button>
 
       {isOpen && (
@@ -55,7 +77,7 @@ export default function Crops({ onSelect }: Props) {
           <div className="fixed inset-0 flex items-center justify-center z-50">
             <div className="bg-white max-w-3xl w-full p-6 rounded-md shadow-lg relative">
               <button
-                onClick={closeModal}
+                onClick={openModal}
                 className="absolute top-3 right-3 text-gray-500 hover:text-gray-800 text-xl font-bold"
                 aria-label="Close modal"
               >
