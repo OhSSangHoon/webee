aee69b94e58fae294556855aa7518dd99a22633d
오상훈 <admin@osanghun-ui-MacBookAir.local>
2025-06-27 16:33:37 +0900

    fix: diagnosis 페이지 SSR localstorage 오류 해결

--- src/app/diagnosis/page.tsx
+++ src/app/diagnosis/page.tsx
@@ -2,6 +2,9 @@
 
 import { AiDiagnosis, AiDoctor } from "@/features";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function Diagnosis() {
   return (
     <div className="flex flex-row justify-center items-center min-h-screen gap-10 my-10">
