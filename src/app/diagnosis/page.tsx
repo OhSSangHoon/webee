c555a30c8ad5baeaf728a876182e873f9b561943
yun yeo joung <da.young.yun13@gmail.com>
2025-07-07 14:39:49 +0900

    fix:질병진단 페이지 크기 조정

--- src/app/diagnosis/page.tsx
+++ src/app/diagnosis/page.tsx
@@ -3,7 +3,7 @@ import { AiDiagnosis, AiDoctor } from "@/features";
 
 export default function Diagnosis() {
   return (
-    <div className="flex flex-row justify-center items-center min-h-screen gap-10 my-10">
+    <div className="flex flex-row justify-center items-center min-h-screen mx-auto gap-10 py-10 lg:max-w-[70%]">
       <ClientOnly fallback={<div>Loading...</div>}>
         <AiDiagnosis />
         <AiDoctor />
