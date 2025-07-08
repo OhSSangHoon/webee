2330d3731bddfe569beedd1b5a1d2f2078a3d131
yun yeo joung <da.young.yun13@gmail.com>
2025-07-08 16:30:47 +0900

    chore: 뉴스 컴포넌트 분리, 사업자 등록증 주소 필수 변경, 벌 질병 한글변환 추가

--- src/shared/types/beeSwitch.ts
+++ src/shared/types/beeSwitch.ts
@@ -11,3 +11,19 @@ export const getBeeTypeKorean = (beeType: string) => {
       return beeType;
   }
 };
+
+//벌 질병 한글 변환 함수
+export const getBeeDiseaseKorean = (beeDisease: string): string => {
+  switch (beeDisease) {
+    case "LARVA_FOULBROOD":
+      return "유충 부저병";
+    case "LARVA_SACBROOD":
+      return "유충 석고병";
+    case "ADULT_VARROA_MITE":
+      return "성충 응애";
+    case "ADULT_WING_DEFORMITY_VIRUS":
+      return "성충 날개불구바이러스감염증";
+    default:
+      return "알 수 없는 질병";
+  }
+};
