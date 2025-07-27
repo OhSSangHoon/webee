023b9a1a5cad9804b5f4511971f32573527048d5
OhSSangHoon <gnsdkaos99@naver.com>
2025-07-27 11:02:58 +0900

    refactor: dynamic import 적용

--- src/app/diagnosis/page.tsx
+++ src/app/diagnosis/page.tsx
@@ -1,4 +1,7 @@
-import { AiDiagnosis, AiDoctor } from "@/features";
+import dynamic from "next/dynamic";
+
+const AiDiagnosis = dynamic(() => import("@/features/AiDiagnosis/ui/AiDiagnosisUi"), { ssr: false });
+const AiDoctor = dynamic(() => import("@/features/AiDoctor/ui/AiDoctUi"), { ssr: false });
 
 export default function Diagnosis() {
   return (
