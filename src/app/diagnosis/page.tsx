dce582cfe49f4dd632bec1a21324243acd3a1604
yun yeo joung <da.young.yun13@gmail.com>
2025-08-01 01:53:19 +0900

    UI: 수정벌 질병진단 ui 수정

--- src/app/diagnosis/page.tsx
+++ src/app/diagnosis/page.tsx
@@ -1,8 +1,8 @@
 import { AiDiagnosisSave } from "@/features";
 export default function Diagnosis() {
   return (
-    <div className="flex flex-row justify-center items-center min-h-screen mx-auto gap-10 py-10 lg:max-w-[70%]">
-        <AiDiagnosisSave />
+    <div className="flex flex-row justify-center items-center min-h-screen mx-auto gap-10 py-10 lg:w-[65%]">
+      <AiDiagnosisSave />
     </div>
   );
 }
