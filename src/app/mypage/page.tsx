b83ce18f527f8778cd387c211e601abfb981a9ad
OhSSangHoon <gnsdkaos99@naver.com>
2025-08-03 23:09:29 +0900

    마이페이지 UI 개선 및 반응형

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -8,10 +8,20 @@ import {
 export default function MyPage() {
   return (
     <div className="lg:w-[65%] flex flex-col gap-8 mx-auto pt-30 pb-10">
-      <div className="flex flex-row gap-6 w-full">
-        <MySaleList />
-        <MyProfile />
+      {/* MySaleList와 MyProfile */}
+      <div className="w-full flex flex-col md:flex-row gap-6">
+        {/* MySaleList */}
+        <div className="w-full md:w-[70%] order-2 md:order-1">
+          <MySaleList />
+        </div>
+        
+        {/* MyProfile */}
+        <div className="w-full md:w-[30%] order-1 md:order-2">
+          <MyProfile />
+        </div>
       </div>
+
+      {/* 추가 컴포넌트들 */}
       <StoreRecommendBee />
       <DiagnosisHistory />
     </div>
